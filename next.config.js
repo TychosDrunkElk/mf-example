6adfad5a6c37ca52683f1a1a07d73b5550ddd6cc
phoebe <phoebe@spotify.com>
2021-08-13 11:19:35 -0400

    use remote bizbirds

--- next.config.js
+++ next.config.js
@@ -1,3 +1,28 @@
 module.exports = {
   reactStrictMode: true,
+  images: {
+    domains: ['i.imgur.com']
+  },
+  webpack(config, options) {
+    config.plugins.push(
+      new options.webpack.container.ModuleFederationPlugin({
+        remoteType: 'var',
+        remotes: {
+          exampleRemote: 'exampleRemote',
+        },
+        shared: {
+          react: {
+            eager: true,
+            singleton: true,
+            requiredVersion: false,
+          },
+        },
+      }),
+    );
+    config.module.rules.push({
+      test: /pages\/_app.[jt]sx?/,
+      loader: '@module-federation/nextjs-mf/lib/federation-loader.js',
+    });
+    return config;
+  }
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,6 @@
+if (process.browser) {
+  window.exampleRemote.init(__webpack_share_scopes__.default);
+}
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
